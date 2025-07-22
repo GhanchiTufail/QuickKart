3cedb88a8685d16188ddc9a302aa6c56c54ea6f5
GhanchiTufail <146692458+GhanchiTufail@users.noreply.github.com>
2025-07-22 09:16:27 +0530

    Update dependency.py

--- source/utils/dependency.py
+++ source/utils/dependency.py
@@ -1,16 +1,28 @@
-from fastapi import Depends, Request
-from sqlalchemy.orm import Session
-from source.config.database import get_db
-from source.models.seller import Seller
-from source.models.product import Product
-from source.utils.token import get_current_seller
+    return product
 
-#seller role
-def get_seller_products(
-    request: Request,
-    db: Session = Depends(get_db),
-    seller: Seller = Depends(get_current_seller)
+def get_all_products(
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).all()   
+    return product    return product
 
+def get_all_products(
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).all()   
+    return product    return product
+
+def get_all_products(
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).all()   
+    return product    return product
+
+def get_all_products(
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).all()   
+    return product
 ):
     product = db.query(Product).filter(Product.seller_id == seller.id).all()   
     return product
